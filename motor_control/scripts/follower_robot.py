db08dee7ae5896c38c104a9d0695db50b450c939
StanleyWang1 <swang11@stanford.edu>
2024-10-11 04:26:42 +0000

    minimum viable follower/leader, needs rebuild

--- motor_control/scripts/follower_robot.py
+++ motor_control/scripts/follower_robot.py
@@ -9,16 +9,25 @@ from motor_control.msg import RRPkinematics
 
 ## -------------------- Dynamixel Configuration --------------------
 # Control table address for Dynamixel
+ADDR_OPERATING_MODE = 11
+ADDR_TORQUE_ENABLE = 64
+ADDR_PROFILE_VELOCITY = 112
 ADDR_GOAL_POSITION   = 116
+# Torque on/off
+TORQUE_ENABLE = 1        # Value for enabling torque
+TORQUE_DISABLE = 0       # Value for disabling torque
 # Protocol 2.0
 PROTOCOL_VERSION        = 2.0
 # Default setting (use appropriate values)
 BAUDRATE                = 57600
 DEVICENAME              = '/dev/ttyUSB0'  # Change to COM3 if on Windows
 # Motor IDs
-ROLL_MOTOR = 4
-PITCH_MOTOR = 5
-EXTENSION_MOTOR = 6
+ROLL_MOTOR = 11
+PITCH_MOTOR = 12
+EXTENSION_MOTOR = 13
+# Reference/home position for zeroing
+ROLL_HOME_POS = 500
+PITCH_HOME_POS = 3855
 
 class FollowerRobot(Node):
     def __init__(self) -> None:
@@ -37,11 +46,33 @@ class FollowerRobot(Node):
             self.get_logger().error("Failed to set baudrate")
             return
         
+        self.set_extended_position_mode(ROLL_MOTOR)
+        self.set_extended_position_mode(PITCH_MOTOR)
+        
+        self.set_profile_velocity(ROLL_MOTOR, 100)
+        self.set_profile_velocity(PITCH_MOTOR, 100)
+        
+        self.enable_motor(ROLL_MOTOR)
+        self.enable_motor(PITCH_MOTOR)
+        self.enable_motor(EXTENSION_MOTOR)
+        
+        self.write_position(ROLL_MOTOR, ROLL_HOME_POS)
+        self.write_position(PITCH_MOTOR, PITCH_HOME_POS)
+        
     def follower_callback(self, msg: RRPkinematics) -> None:
-        self.write_position(ROLL_MOTOR, msg.roll_angle)
-        self.write_position(PITCH_MOTOR, msg.pitch_angle)
-        self.write_position(EXTENSION_MOTOR, msg.extension_angle)
+        self.write_position(ROLL_MOTOR, msg.roll_angle + ROLL_HOME_POS)
+        self.write_position(PITCH_MOTOR, msg.pitch_angle + PITCH_HOME_POS)
+        # self.write_position(EXTENSION_MOTOR, msg.extension_angle)
     
+    def set_extended_position_mode(self, motor_id) -> None:
+        result, error = self.packet_handler.write1ByteTxRx(self.port_handler, motor_id, ADDR_OPERATING_MODE, 4)
+        if result != dxl.COMM_SUCCESS:
+            self.get_logger().error(f"Error setting extended position mode for motor {motor_id}")
+        elif error != 0:
+            print(f"Dynamixel error: {self.packet_handler.getRxPacketError(error)}")
+        else:
+            print(f"Set motor {motor_id} to extended position mode!")
+            
     def write_position(self, motor_id, position):
         # Read present position of the Dynamixel motor
         result, error = self.packet_handler.write4ByteTxRx(self.port_handler, motor_id, ADDR_GOAL_POSITION, position)
@@ -53,11 +84,33 @@ class FollowerRobot(Node):
             self.get_logger().info(f"Set Motor {motor_id} to position: {position}")
         return position
 
+    def set_profile_velocity(self, motor_id, profile_velocity) -> None:
+        result, error = self.packet_handler.write4ByteTxRx(self.port_handler, motor_id, ADDR_PROFILE_VELOCITY, profile_velocity)
+        # ADD ERROR HANDLING HERE
+        
+    def enable_motor(self, motor_id) -> None:
+        result, error = self.packet_handler.write1ByteTxRx(self.port_handler, motor_id, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        if result != dxl.COMM_SUCCESS:
+            print(f"Error enabling torque: {self.packet_handler.getTxRxResult(result)}")
+        elif error != 0:
+            print(f"Dynamixel error: {self.packet_handler.getRxPacketError(error)}")
+        else:
+            print("Torque enabled")
+            
+    def disable_motor(self, motor_id) -> None:
+        result, error = self.packet_handler.write1ByteTxRx(self.port_handler, motor_id, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        if result != dxl.COMM_SUCCESS:
+            print(f"Error disabling torque: {self.packet_handler.getTxRxResult(result)}")
+        elif error != 0:
+            print(f"Dynamixel error: {self.packet_handler.getRxPacketError(error)}")
+        else:
+            print("Torque disabled")
+            
     def close_port(self):
         # Disable motors and close the port when done
-        # self.disable_motor(ROLL_MOTOR)
-        # self.disable_motor(PITCH_MOTOR)
-        # self.disable_motor(EXTENSION_MOTOR)
+        self.disable_motor(ROLL_MOTOR)
+        self.disable_motor(PITCH_MOTOR)
+        self.disable_motor(EXTENSION_MOTOR)
         self.port_handler.closePort()
         
 if __name__ == "__main__":
--- motor_control/scripts/leader_robot.py
+++ motor_control/scripts/leader_robot.py
@@ -9,22 +9,29 @@ from motor_control.msg import RRPkinematics
 
 ## -------------------- Dynamixel Configuration --------------------
 # Control table address for Dynamixel
+ADDR_TORQUE_ENABLE = 64
 ADDR_PRESENT_POSITION   = 132
+# Torque on/off
+TORQUE_ENABLE = 1        # Value for enabling torque
+TORQUE_DISABLE = 0       # Value for disabling torque
 # Protocol 2.0
 PROTOCOL_VERSION        = 2.0
-# Default setting (use appropriate values)
+# Connection settings (use appropriate values)
 BAUDRATE                = 57600
 DEVICENAME              = '/dev/ttyUSB0'  # Change to COM3 if on Windows
 # Motor IDs
 ROLL_MOTOR = 1
 PITCH_MOTOR = 2
 EXTENSION_MOTOR = 3
+# Reference/home position for zeroing
+ROLL_HOME_POS = 2085
+PITCH_HOME_POS = 2030
 
 class LeaderRobot(Node):
     def __init__(self) -> None:
         super().__init__("LeaderRobot")
         self.leader_pub = self.create_publisher(RRPkinematics, "/LeaderKinematics", 10)
-        self.leader_timer = self.create_timer(0.1, self.leader_callback)
+        self.leader_timer = self.create_timer(0.05, self.leader_callback)
 
         # Initialize PortHandler and PacketHandler
         self.port_handler = dxl.PortHandler(DEVICENAME)
@@ -40,12 +47,12 @@ class LeaderRobot(Node):
         
     def leader_callback(self) -> None:
         msg = RRPkinematics()
-        msg.roll_angle = self.read_position(ROLL_MOTOR)
-        msg.pitch_angle = self.read_position(PITCH_MOTOR)
+        msg.roll_angle = self.read_position(ROLL_MOTOR) - ROLL_HOME_POS
+        msg.pitch_angle = self.read_position(PITCH_MOTOR) - PITCH_HOME_POS
         msg.extension_angle = self.read_position(EXTENSION_MOTOR)
         self.leader_pub.publish(msg)
     
-    def read_position(self, motor_id):
+    def read_position(self, motor_id) -> None:
         # Read present position of the Dynamixel motor
         position, result, error = self.packet_handler.read4ByteTxRx(self.port_handler, motor_id, ADDR_PRESENT_POSITION)
         if result != dxl.COMM_SUCCESS:
@@ -55,12 +62,30 @@ class LeaderRobot(Node):
         else:
             self.get_logger().info(f"Motor {motor_id} position: {position}")
         return position
-
+    
+    def enable_motor(self, motor_id) -> None:
+        result, error = self.packet_handler.write1ByteTxRx(self.port_handler, motor_id, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        if result != dxl.COMM_SUCCESS:
+            print(f"Error enabling torque: {self.packetHandler.getTxRxResult(result)}")
+        elif error != 0:
+            print(f"Dynamixel error: {self.packetHandler.getRxPacketError(error)}")
+        else:
+            print("Torque enabled")
+            
+    def disable_motor(self, motor_id) -> None:
+        result, error = self.packet_handler.write1ByteTxRx(self.port_handler, motor_id, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        if result != dxl.COMM_SUCCESS:
+            print(f"Error disabling torque: {self.packetHandler.getTxRxResult(result)}")
+        elif error != 0:
+            print(f"Dynamixel error: {self.packetHandler.getRxPacketError(error)}")
+        else:
+            print("Torque disabled")
+        
     def close_port(self):
         # Disable motors and close the port when done
-        # self.disable_motor(ROLL_MOTOR)
-        # self.disable_motor(PITCH_MOTOR)
-        # self.disable_motor(EXTENSION_MOTOR)
+        self.disable_motor(ROLL_MOTOR)
+        self.disable_motor(PITCH_MOTOR)
+        self.disable_motor(EXTENSION_MOTOR)
         self.port_handler.closePort()
         
 if __name__ == "__main__":
